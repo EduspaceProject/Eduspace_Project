0b1a9381e296b78c02210d0a6849a6ef5d246bfd
Akshaykumarsinghal <59149089+Akshaykumarsinghal@users.noreply.github.com>
2020-10-02 15:31:20 +0530

    coomit

--- Eduspace/student/urls.py
+++ Eduspace/student/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views.login import Login
+from .views.login import Login,logout
 from .views.register import Register
 from .views.home import home
 urlpatterns = [
     path('',home,name='homepage'),
     path('login/',Login.as_view(),name='login'),
-    path('register/',Register.as_view(),name='register')
+    path('register/',Register.as_view(),name='register'),
+    path('logout/',logout,name='logout')
 
 ]
